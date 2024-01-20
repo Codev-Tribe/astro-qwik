88830cbb2fb3045ab876e7ea0a072da506cac4e5
Eduard Ramirez <edd.ramirez@proton.me>
2024-01-20 00:13:34 -0500

    Update astro.config.mjs to use "hybrid" output and add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import qwikdev from "@qwikdev/astro";
 import tailwind from "@astrojs/tailwind";
 
@@ -7,6 +7,6 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   integrations: [qwikdev(), tailwind()],
-  output: "server",
-  adapter: vercel()
+  output: "hybrid",
+  adapter: vercel(),
 });
